bce6c99fcb1eb6f2e326484ee4f6b2a5399c1a05
Ondalys <helmf@kth.se>
2019-11-01 17:53:36 +0100

    Made a window

--- ChampionsOfITK.py
+++ ChampionsOfITK.py
@@ -1,1 +1,8 @@
-print("Hail stan");
+from kivy.app import App
+from kivy.uix.button import Button
+
+class ChampionsOfITK(App):
+    def build(self):
+        return Button(text="Champions of ITK!!!")
+
+ChampionsOfITK().run()
