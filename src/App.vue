ac52d2f97c24da94bf0c8f6e40cb9db9ff9fbeb1
Ondalys <helmf@kth.se>
2020-01-29 18:36:54 +0100

    Add few first changes for champions of ITK

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="ITK logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Champions of ITK"/>
   </div>
 </template>
 
