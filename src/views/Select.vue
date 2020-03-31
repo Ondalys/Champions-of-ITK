1e7d058ff90789acb78ceeb68de978e8600a3900
Ackuq@ <axel@pettersson.cc>
2020-03-31 23:18:06 +0200

    Started select screen

--- src/views/Select.vue
+++ src/views/Select.vue
@@ -1,3 +1,28 @@
+<style scoped>
+.button-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+.button-container > a,
+button {
+    margin: 10px 0px;
+}
+</style>
 <template>
-    <div>HEIFLKNHAKLNH</div>
+    <div class="container">
+        <div>Your adventure starts here!</div>
+        <div class="button-container">
+            <v-btn to="decision">Start new adventure</v-btn>
+            <v-btn disabled>Load game</v-btn>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'select',
+    components: {}
+};
+</script>
